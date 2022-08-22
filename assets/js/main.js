9dd493bfa6718663feeff876c742749513281a76
Yama <gabriel.yamashita01@gmail.com>
2022-08-22 00:47:57 -0300

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,7 +50,7 @@ skillsHeader.forEach((el) => {
 
 /*============== QUALIFICATION TABS ==============*/
 const tabs = document.querySelectorAll('[data-target]'),
-      tabContents = document.querySelectorAll('[data-content]')
+   tabContents = document.querySelectorAll('[data-content]')
 
 tabs.forEach(tab => {
    tab.addEventListener('click', () => {
@@ -142,19 +142,19 @@ submit.addEventListener('click', () => {
 const sections = document.querySelectorAll('section[id]')
 
 function scrollActive() {
-    const scrollY = window.pageYOffset
+   const scrollY = window.pageYOffset
 
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
+   sections.forEach(current => {
+      const sectionHeight = current.offsetHeight
+      const sectionTop = current.offsetTop - 50;
+      sectionId = current.getAttribute('id')
 
-        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else {
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-        }
-    })
+      if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+         document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+      } else {
+         document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+      }
+   })
 }
 window.addEventListener('scroll', scrollActive)
 
