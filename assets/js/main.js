60ebc16a5e2f2dc2c7bb37926fe9d334065fb504
Yama <gabriel.yamashita01@gmail.com>
2023-05-30 00:03:08 -0300

    atualizado 23.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,9 +23,9 @@ if (navClose) {
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction() {
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
+   const navMenu = document.getElementById('nav-menu')
+   // When we click on each nav__link, we remove the show-menu class
+   navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
@@ -140,6 +140,7 @@ submit.addEventListener('click', () => {
 
 /*=========== SCROLL SECTIONS ACTIVE LINK ==========*/
 const sections = document.querySelectorAll('section[id]')
+console.log(sections)
 
 function scrollActive() {
    const scrollY = window.pageYOffset
